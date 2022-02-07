fbb6ed8171965b63c64f8c997d0d2f0c3ddee113
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2022-02-07 17:31:07 +0330

    test cards ends

---
+++ front/src/CardTest.js
@@ -0,0 +1,95 @@
+import "./App.css";
+import _Card_ from "./common/Card";
+import Grid from "@material-ui/core/Grid";
+
+function App() {
+  return (
+    <div className="App">
+      <Grid container spacing={25} className="margin-f center">
+        <Grid item xs={4} className="margin-top">
+          <_Card_
+            profile_name="SF"
+            title="FOOD TITLE"
+            subheader="FOOD SUBHEADER"
+            body="BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY"
+            image="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          />
+        </Grid>
+        <Grid item xs={4} className="margin-top">
+          <_Card_
+            profile_name="SF"
+            title="FOOD TITLE"
+            subheader="FOOD SUBHEADER"
+            body="BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY"
+            image="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          />
+        </Grid>
+        <Grid item xs={4} className="margin-top">
+          <_Card_
+            profile_name="SF"
+            title="FOOD TITLE"
+            subheader="FOOD SUBHEADER"
+            body="BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY"
+            image="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          />
+        </Grid>
+        <Grid item xs={4} className="margin-top">
+          <_Card_
+            profile_name="SF"
+            title="FOOD TITLE"
+            subheader="FOOD SUBHEADER"
+            body="BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY"
+            image="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          />
+        </Grid>
+        <Grid item xs={4} className="margin-top">
+          <_Card_
+            profile_name="SF"
+            title="FOOD TITLE"
+            subheader="FOOD SUBHEADER"
+            body="BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY"
+            image="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          />
+        </Grid>
+        <Grid item xs={4} className="margin-top">
+          <_Card_
+            profile_name="SF"
+            title="FOOD TITLE"
+            subheader="FOOD SUBHEADER"
+            body="BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY"
+            image="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          />
+        </Grid>
+        <Grid item xs={4} className="margin-top">
+          <_Card_
+            profile_name="SF"
+            title="FOOD TITLE"
+            subheader="FOOD SUBHEADER"
+            body="BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY"
+            image="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          />
+        </Grid>
+        <Grid item xs={4} className="margin-top">
+          <_Card_
+            profile_name="SF"
+            title="FOOD TITLE"
+            subheader="FOOD SUBHEADER"
+            body="BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY"
+            image="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          />
+        </Grid>
+        <Grid item xs={4} className="margin-top">
+          <_Card_
+            profile_name="SF"
+            title="FOOD TITLE"
+            subheader="FOOD SUBHEADER"
+            body="BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY BOOOOOOOODY"
+            image="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
